e3b5e7e975ccd1fc7d36e28586c4994ef6a07d86
jacqie williamson <jacqieq@me.com>
2021-06-27 22:42:05 +0100

    add questions

---
+++ assets/javascript/quizquestions.js
@@ -0,0 +1,30 @@
+// create variables
+var questions = [
+    {
+    question: "In programming, 'JS' stands for?",
+    options: ["a. JavaScripted", "b. JavaScripting", "c. JavaScript", "d. Javas"],
+    answer: "c. JavaScript",
+
+        },    
+
+    {
+    question: "In the html file, the JavaScript link goes?",
+    options: ["a. only in the head section", "b. only in the body section", "c. anywhere ", "d. in the head or before the closing body tag"],
+    answer: "d. in the head or before the closing body tag",
+    
+        },
+        
+    {
+    question: "JS can be linked to the html file with which tag?",
+    options: ["a. <a></a>", "b. <script></script>", "c. <link rel='javascript' href'./javascript.js>", "d. can be done with all of there tags"],
+    answer: "b. <script></script> ",
+        
+        },
+
+    {
+    question: "Which of these variable names are valid?",
+    options: ["a. var = 7camels", "b. var = 7.camels", "c. var = sevenCamels", "d. var = 7-camels"],
+    answer: "c. var = sevenCamels",
+            
+        },
+]
